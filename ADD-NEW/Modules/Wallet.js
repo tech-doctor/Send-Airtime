7bed807413a3556bb30d38e54b3a5c4d108c7b6d
tech-doctor <55724699+tech-doctor@users.noreply.github.com>
2021-01-27 17:04:43 +0100

    update the wallet page

--- ADD-NEW/Modules/Wallet.js
+++ ADD-NEW/Modules/Wallet.js
@@ -1,21 +1,80 @@
 const Wallet = () =>{
+   
+ const mainView = document.querySelector('.main-view')
+
+// const tryFunction = (PhoneNumber) =>{
+//   db.collection('create').get().then((snapshot) => {
+//     snapshot.forEach(doc => {
+//       const {PhoneNumber} = doc.data()
+//      console.log(PhoneNumber)
+//     })   
+// })
+
+// }
+db.collection('create').get().then((snapshot) => {
+  snapshot.forEach(doc => {
+    const {PhoneNumber} = doc.data()
+   console.log(PhoneNumber)
+   updateWallet(PhoneNumber)
+  })   
+})
+  
+
+  const  updateWallet = (PhoneNumber) => {
+    
+    const result = `
+    <div class="flex-view">
+      <div class="wallet-btn-div">
+        <div class= "wallet-balance">
+          <p class="heading">Wallet balance:</p>
+          <div class="details">
+            <p class="balance">#700.00</p>
+            <p class = "currency">NGN.</p>
+          </div>
+        </div>
+        <div class="button-div">
+          <button class="create">Create Wallet</button>
+          <button class="fund">Fund wallet</button>
+        </div>
+      </div>
+      <div class="self-balance">
+        <p class="heading">Bank balance:</p>
+        <p class="details"> 
+          <i class="fas fa-money-check-alt"></i>
+          <br><br>Your  bank balance will be displayed here <br>once you create a wallet
+        </p>
+        <p class="balance">#1588394.00</p>
+        <p class="currency">NGN</p>
+      </div>
+    </div>	
+    <div class="message-div">
+      <p class="message"> After creating a wallet, a phone number would be generated here. The generated phone number will be required  for  funding your wallet.</p>
+      <p class="phone-number"> Phone-number: <span id="number">${PhoneNumber}  <i class="far fa-copy"> copy</i></span></p>
+      
+      </div> ` 
+    return result;
+  }
+
+ mainView.innerHTML += updateWallet()
+
+
   const createWalletButon = document.querySelector('.button-div .create');
   const fundWalletButton = document.querySelector('.button-div .fund');
   const modal = document.querySelectorAll('.modal');
   const closeModal = document.querySelectorAll('.close')
 
-  //open create wallet modal
+  /////////////////////////////////////////////////////////////open create wallet modal/////////////////////////////////////////////////
   createWalletButon.addEventListener( 'click', (event) =>{
   modal[1].style.display = 'block'
   })
 
-  //open fund wallet modal
+  //////////////////////////////////////////////////open fund wallet modal///////////////////////////////////////////////////////
   fundWalletButton.addEventListener( 'click', (event) => {
   modal[0].style.display = 'block'
   })
 
 
-  //to close the modal
+  ///////////////////////////////////////////////////////////to close the modal//////////////////////////////////////////////
   window.onclick = (event) => {
     close(event);
   }
@@ -27,9 +86,8 @@ const Wallet = () =>{
     }
   }
 
-  ////copy number to clipboard /////////////////////////
+  ////////////////////////////////////////////////////////////////copy number to clipboard ///////////////////////////////////////////////////////
   
-
   const copyIcon = document.querySelectorAll('.fa-copy')[1]
   copyIcon.addEventListener ('click', (e) => {
     e.preventDefault()
@@ -43,7 +101,7 @@ const Wallet = () =>{
     document.body.removeChild(aux);
   })
  
-  ///////////////////////////////////////// Submit generate/create wallet form. ///////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////// Submit generate/create wallet form. ///////////////////////////////////////////////////////
   const generateForm = document.querySelector('#generateForm')
   generateForm.addEventListener ('submit', (e) =>{
     e.preventDefault();
@@ -59,8 +117,20 @@ const Wallet = () =>{
       firstName, lastName, email, DOB
     }      
   }
-
-/////////Send create wallet data
+////////////////////////////////////////////////////////////send created wallet data to firebase //////////////////////////////////////////
+const sendCreateWallet = (AccountNo, PhoneNumber, Bank) => {
+  db.collection('create').add({
+    AccountNo: AccountNo,
+    Bank: Bank,
+    PhoneNumber: PhoneNumber
+  }).then(()=> {
+    loadingOverlay.style.display = 'none' 
+  })
+  
+}
+////////////////////////////////////////////////////////Send create wallet data  //////////////////////////////////////////////
+const loadingOverlay  = document.querySelector('.loading-overlay')
+const walletSuccess = document.querySelector('.wallet-success')
   function postCreatewalletData(){
     createFieldValue()
     console.log(firstName.value)
@@ -80,7 +150,8 @@ const Wallet = () =>{
     'Content-Type' : 'application/json',
     'Authorization': `Bearer ${publicKey}`
     }
-
+    modal[1].style.display = 'none'
+    loadingOverlay.style.display = 'block'
     fetch(proxy + url, {
       method: "POST",
       headers: headers,
@@ -88,11 +159,83 @@ const Wallet = () =>{
       redirect:"follow" 
     })
     .then(result => result.json())
-    .then(resp => console.log(resp))
-    .catch(err => console.log('Error : ',err))
+    .then(resp => {
+      console.log(resp)
+      const {Message, ResponseCode} = resp.Response;
+      const {AccountNo, PhoneNumber, Bank} = resp.Data;
+      bankBalance()
+      sendCreateWallet(AccountNo, PhoneNumber, Bank)
+
+      
+      if(ResponseCode == 200){
+        walletSuccess.innerHTML = `<p>${Message}</p>`
+        walletSuccess.style.display = 'block'
+        timeOut()
+      }else{
+        walletSuccess.innerHTML = `<p>${Message}</p>`
+        walletSuccess.style.backgroundColor = '#C21807'
+        walletSuccess.style.display = 'block'
+        timeOut()
+      } 
+    })
+    .catch(err => {
+       console.log(err)
+       loadingOverlay.style.display = 'none'
+       walletSuccess.innerHTML = `<p>Something went wrong, please try again</p>`
+       walletSuccess.style.backgroundColor = '#C21807'
+       walletSuccess.style.display = 'block'
+       timeOut()
+    })  
   }  
 
-/// Submit Fund form wallet to fund wallet.
+  ///////Settime out after 3 seconds/////////
+  const timeOut = () => {
+    setTimeout  (() => {
+      walletSuccess.style.display = "none"
+    },3000)	 
+  }
+
+  ///////////////////////////////////////////////Bank balance POST request//////////////////////////////////////////
+
+  const bankBalance = () => {
+    const data = {
+      "Currency": "NGN",
+	     "SecretKey": "hfucj5jatq8h"
+    }
+
+      const url = "https://sandbox.wallets.africa/self/balance";
+      const publicKey = "uvjqzm5xl6bw";
+      const proxy = "https://cors-anywhere.herokuapp.com/"
+      const headers = {
+      'Content-Type' : 'application/json',
+      'Authorization': `Bearer ${publicKey}`
+      }
+      
+      fetch(proxy + url, {
+        method: "POST",
+        headers: headers,
+        body : JSON.stringify(data),
+        redirect:"follow" 
+      })
+      .then(result => result.json())
+      .then(resp => {
+        console.log(resp)
+        db.collection('balance').add({
+          BankBalance: resp.Data.WalletBalance
+        }).then(()=> {
+          loadingOverlay.style.display = 'none' 
+        })
+        
+      })
+      .catch(err => {
+         console.log(err)
+        
+      })  
+  }
+
+  
+
+////////////////////////////////////////////////////////////// Submit Fund form wallet to fund wallet. ////////////////////////////////////////////
 const fundForm =document.querySelector('#fundForm')
 fundForm.addEventListener ('submit', (e) => {
   e.preventDefault();
@@ -124,6 +267,8 @@ fundForm.addEventListener ('submit', (e) => {
     'Content-Type' : 'application/json',
     'Authorization': `Bearer ${publicKey}`
     }
+    modal[0].style.display = 'none'
+    loadingOverlay.style.display = 'block'
     fetch(proxy + url, {
       method: "POST",
       headers: headers,
@@ -131,48 +276,49 @@ fundForm.addEventListener ('submit', (e) => {
       redirect:"follow" 
     })
     .then(result => result.json())
-    .then(resp => console.log(resp))
-    .catch(err => console.log('Error : ',err))
+    .then(resp => {
+      console.log(resp)
+      const {Message, ResponseCode} = resp.Response;
+      const {AmountCredited, RecipientWalletBalance,SenderWalletBalance} = resp.Data
+      sendFundWallet(AmountCredited, RecipientWalletBalance,SenderWalletBalance)
+      loadingOverlay.style.display = 'none'
+      bankBalance()
+      if(ResponseCode == 200){
+        walletSuccess.innerHTML = `<p>${Message}</p>`
+        walletSuccess.style.display = 'block'
+        timeOut()
+      }else{
+        walletSuccess.innerHTML = `<p>Error, Try again!</p>`
+        walletSuccess.style.backgroundColor = '#C21807'
+        walletSuccess.style.display = 'block'
+        timeOut()
+      } 
+    })
+    .catch(err => {
+       console.log(err)
+       loadingOverlay.style.display = 'none'
+       walletSuccess.innerHTML = `<p>Something went wrong, please try again</p>`
+       walletSuccess.style.backgroundColor = '#C21807'
+       walletSuccess.style.display = 'block'
+       timeOut()
+    })  
+    
   }
 
-  // const mainView = document.querySelector('.main-view')
-
-  // const  updateWallet = () => {
-  //   const result = `
-  //   <div class="flex-view">
-  //     <div class="wallet-btn-div">
-  //       <div class= "wallet-balance">
-  //         <p class="heading">Wallet balance:</p>
-  //         <div class="details">
-  //           <p class="balance">#700.00</p>
-  //           <p class = "currency">NGN.</p>
-  //         </div>
-  //       </div>
-  //       <div class="button-div">
-  //         <button class="create">Create Wallet</button>
-  //         <button class="fund">Fund wallet</button>
-  //       </div>
-  //     </div>
-  //     <div class="self-balance">
-  //       <p class="heading">Bank balance:</p>
-  //       <p class="details"> 
-  //         <i class="fas fa-money-check-alt"></i>
-  //         <br><br>Your  bank balance will be displayed here <br>once you create a wallet
-  //       </p>
-  //       <p class="balance">#1588394.00</p>
-  //       <p class="currency">NGN</p>
-  //     </div>
-  //   </div>	
-  //   <div class="message-div">
-  //     <p class="message"> After creating a wallet, a phone number would be generated here. The generated phone number will be required  for  funding your wallet.</p>
-  //     <p class="phone-number"> Phone-number: <span id="number"> 08067883093 <i class="far fa-copy"> copy</i></span></p>
-  //   </div> ` 
-  //   return result;
-  // }
-
- // mainView.innerHTML += updateWallet()
+  const sendFundWallet = (AmountCredited, RecipientWalletBalance,SenderWalletBalance) => {
+    db.collection('Fund').add({
+      BalanceAdded:  AmountCredited,
+      TotalBalance: RecipientWalletBalance, 
+    }).then(()=> {
+      loadingOverlay.style.display = 'none' 
+    })
+  
+  }
 
   
+
+
+
 
 }
 
